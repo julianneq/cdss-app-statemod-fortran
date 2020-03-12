0d41fb326ad941700528b62bb3e72db3db79a49d
julianneq <jquinn@DESKTOP-Q2B5352.localdomain>
2020-03-12 19:28:50 -0400

    added code for Figures S6 and S8

--- UCRB_analysis/Figures_Code/makeFigureS6_ReconstructionParams.py
+++ UCRB_analysis/Figures_Code/makeFigureS6_ReconstructionParams.py
@@ -1,7 +1,80 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 25 16:33:24 2020
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import seaborn as sns
+import scipy.stats as ss
+from matplotlib import pyplot as plt
+from utils import fitHMM
 
-@author: jquinn
-"""
+# load paleo data at Cisco
+Paleo = pd.read_csv('../Qgen/Reconstruction/Cisco_Recon_v_Observed_v_Stateline.csv')
 
+# re-scale Cisco data to estimate data at CO-UT state line
+factor = np.nanmean(Paleo['ObservedNaturalStateline']/Paleo['ObservedNaturalCisco'])
+Paleo['ScaledNaturalCisco'] = Paleo['ObservedNaturalCisco']*factor
+Paleo['ScaledReconCisco'] = Paleo['ReconCisco']*factor
+
+# compute residual between observed stateline flow and scaled reconstructed flow
+Paleo['ScalingResid'] = Paleo['ObservedNaturalStateline'] - Paleo['ScaledReconCisco']
+Paleo['FractionScalingResid'] = Paleo['ScalingResid']/Paleo['ScaledReconCisco']
+
+# fit HMM to true observations of flows at state line over historical record
+def fitParams(flows):
+    # create matrices to store the parameters
+    # each row is a different simulation
+    # columns are mu0, sigma0, mu1, sigma1, p00, p11
+    params = np.zeros([6])
+    hidden_states, mus, sigmas, P = fitHMM(np.log(flows))
+    #makePPandQQplot(np.log(flows), mus, sigmas, P, 'No Noise Recon Fit ', 'ReconFits/NoNoiseFit')
+    
+    params[0] = mus[0]
+    params[1] = sigmas[0]
+    params[2] = mus[1]
+    params[3] = sigmas[1]
+    params[4] = P[0,0]
+    params[5] = P[1,1]
+        
+    return params
+
+trueParams = np.array([[15.258112, 0.259061, 15.661007, 0.252174, 0.679107, 0.649169]])
+trueParams = pd.DataFrame({'mu0':trueParams[:,0],'sigma0':trueParams[:,1],\
+                           'mu1':trueParams[:,2],'sigma1':trueParams[:,3],\
+                           'p00':trueParams[:,4],'p11':trueParams[:,5]})
+trueParams['Ensemble'] = 'Observations'
+
+# fit HMM to scaled, reconstructed Cisco flows to represent estimate of flow at state line
+# after first adding random errors from residuals of scaled, reconstructed Cisco flows vs. CO-UT state line flows
+# repeat 100 times and see how estimates compare with those over the historical record itself
+nsims = 100
+simParams = np.zeros([nsims,6])
+stdev = np.std(Paleo['FractionScalingResid'][340:429])
+for i in range(nsims):
+    flows = Paleo['ScaledReconCisco'][340:429] + Paleo['ScaledReconCisco'][340:429]*ss.norm.rvs(0,stdev,429-340)
+    simParams[i,:] = fitParams(np.array(flows))
+
+# make scatter plot of fitted params to observations (trueParams) compared to
+# fitted params to scaled reconstructed flows + residuals (simParams) and their average
+means = np.mean(simParams,0)
+simParams = pd.DataFrame({'mu0':simParams[:,0],'sigma0':simParams[:,1],\
+                          'mu1':simParams[:,2],'sigma1':simParams[:,3],\
+                          'p00':simParams[:,4],'p11':simParams[:,5]})
+simParams['Ensemble'] = 'Reconstruction + Noise'
+
+noNoiseParams = fitParams(np.array(Paleo['ScaledReconCisco'][340:429]))
+
+simParams.loc[nsims] = [means[0], means[2], means[4], means[5], means[1], means[3], 'MeanRecon+Noise']
+simParams.loc[nsims+1] = [noNoiseParams[0], noNoiseParams[2], noNoiseParams[4], \
+              noNoiseParams[5], noNoiseParams[1], noNoiseParams[3], 'Recon']
+
+allParams = pd.concat([simParams,trueParams])
+
+
+sns.set_style("dark")
+
+colors=['#ff7f00','#377eb8','#4daf4a','#000000']
+sns.set_palette(sns.color_palette(colors))
+col = allParams.columns.tolist()
+
+fig = sns.pairplot(allParams,hue='Ensemble',corner=True)            
+plt.savefig('FigureS6_ReconstructionParams.pdf')
+plt.clf()
--- UCRB_analysis/Figures_Code/makeFigureS8_VICparams.py
+++ UCRB_analysis/Figures_Code/makeFigureS8_VICparams.py
@@ -1,7 +1,101 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 25 16:34:02 2020
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import seaborn as sns
+import scipy.stats as ss
+from matplotlib import pyplot as plt
+from utils import fitHMM
 
-@author: jquinn
-"""
+AnnualQ_h = np.loadtxt('../Qgen/AnnualQ.csv',delimiter=',',skiprows=1,usecols=[208])*1233.48 # convert to m^3
 
+# ignore first 41 years (1909-1949)
+AnnualQ_h = AnnualQ_h[41::]
+
+CMIPscenarios = np.loadtxt('../Qgen/CMIP/CMIP3_CMIP5_singletrace/CMIP_monthlyQ_m3.csv',delimiter=',',skiprows=1,usecols=[*range(1,99)])
+
+# compute annual flows under CMIP scenarios
+CMIP_annual = np.zeros([64,98])
+for i in range(64):
+    CMIP_annual[i,:] = np.sum(CMIPscenarios[i*12:(i+1)*12,:],0)
+    
+# log-space residuals divided by mean log-space prediction
+log_resid = np.log(AnnualQ_h) - np.log(CMIP_annual[:,0])
+log_resid_frac = log_resid/np.mean(np.log(CMIP_annual[:,0]))
+
+mu_resid = np.mean(log_resid_frac)
+std_resid = np.std(log_resid_frac)
+rho1_resid = np.corrcoef(log_resid_frac[1:],log_resid_frac[0:-1])[0,1]
+
+def fitParams(flows):
+    # create matrices to store the parameters
+    # each row is a different simulation
+    # columns are mu0, sigma0, mu1, sigma1, p00, p11
+    params = np.zeros([6])
+    hidden_states, mus, sigmas, P = fitHMM(np.log(flows))
+    
+    params[0] = mus[0]
+    params[1] = sigmas[0]
+    params[2] = mus[1]
+    params[3] = sigmas[1]
+    params[4] = P[0,0]
+    params[5] = P[1,1]
+        
+    return params
+
+# 1) take log-space CMIP simulations
+# 2) take mean and variance of normal distribution fitted above
+# 3) multiply by mean log-space CMIP prediction
+# 4) generate normal AR1 noise from that distribution and add it to log-space CMIP simulations
+# 5) fit HMM 
+# 6) repeat 100 times and find mean parameter estimates
+    
+# find mean parameter estimates over nsims of historical CMIP runs 
+# and compare parameter with true parameter estimates over historical data
+nsims = 100
+simParams = np.zeros([nsims,6])
+
+logFlows = np.log(CMIP_annual[:,0])
+noise = np.zeros(len(logFlows))
+mu_noise = mu_resid*np.mean(logFlows)
+std_noise = std_resid*np.mean(logFlows)
+for j in range(nsims): # add noise and fit HMM 100x
+    noise[0] = ss.norm.rvs(mu_noise,std_noise)
+    for k in range(len(logFlows)-1):
+        noise[k+1] = mu_noise + rho1_resid*(noise[k] - mu_noise) + \
+                        ss.norm.rvs(0,std_noise)*np.sqrt(1-rho1_resid**2)
+    noisyLogFlows = np.exp((logFlows + noise))/1233.48 # convert to real-space and then acre-ft
+    simParams[j,:] = fitParams(noisyLogFlows)
+    
+baseParams = np.array([fitParams(AnnualQ_h/1233.48)])
+baseParams = pd.DataFrame({'mu0':baseParams[:,0],'sigma0':baseParams[:,1],\
+                          'mu1':baseParams[:,2],'sigma1':baseParams[:,3],\
+                          'p00':baseParams[:,4],'p11':baseParams[:,5]})
+baseParams['Ensemble'] = 'Historical'
+
+noNoiseParams = np.array([fitParams(CMIP_annual[:,0]/1233.48)])
+noNoiseParams = pd.DataFrame({'mu0':noNoiseParams[:,0],'sigma0':noNoiseParams[:,1],\
+                          'mu1':noNoiseParams[:,2],'sigma1':noNoiseParams[:,3],\
+                          'p00':noNoiseParams[:,4],'p11':noNoiseParams[:,5]})
+noNoiseParams['Ensemble'] = 'VIC Predictions'
+
+meanParams = np.array([np.mean(simParams,0)])
+meanParams = pd.DataFrame({'mu0':meanParams[:,0],'sigma0':meanParams[:,1],\
+                          'mu1':meanParams[:,2],'sigma1':meanParams[:,3],\
+                          'p00':meanParams[:,4],'p11':meanParams[:,5]})
+meanParams['Ensemble'] = 'Mean VIC Predictions + Noise'
+    
+simParams = pd.DataFrame({'mu0':simParams[:,0],'sigma0':simParams[:,1],\
+                          'mu1':simParams[:,2],'sigma1':simParams[:,3],\
+                          'p00':simParams[:,4],'p11':simParams[:,5]})
+simParams['Ensemble'] = 'VIC Predictions + Noise'
+
+allSamples = pd.concat([simParams,meanParams,noNoiseParams,baseParams])
+
+sns.set_style("dark")
+
+colors=['#ff7f00','#377eb8','#4daf4a','#000000']
+sns.set_palette(sns.color_palette(colors))
+
+sns.pairplot(allSamples,hue='Ensemble')
+plt.savefig('FigureS8_VICparams.pdf')
+plt.clf()
